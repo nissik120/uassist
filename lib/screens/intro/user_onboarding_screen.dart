1beb6570e2a60c9509c68511d753dada9dcd0da6
nissik120 <nissik120@gmail.com>
2022-02-06 23:42:54 +0100

    cleaning up

--- lib/screens/intro/user_onboarding_screen.dart
+++ lib/screens/intro/user_onboarding_screen.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tuple/tuple.dart';
 import 'package:uassist/delegates/sliver_persistent_header_delegate_impl.dart';
-import 'package:uassist/model/user.dart';
 import 'package:uassist/screens/prime/prime_widgets/prime_widgets_headers.dart';
 import 'user_onboarding_info_widget.dart';
 
